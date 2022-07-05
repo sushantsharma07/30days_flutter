4ee3834c155e785280a3a6797e70793b389c7516
Amit sharma <amitaery07@gmail.com>
2022-07-05 23:57:01 +0530

    day13 completed

--- first_project/lib/pages/home_page.dart
+++ first_project/lib/pages/home_page.dart
@@ -2,11 +2,35 @@ import 'package:first_project/pages/models/catelog.dart';
 import 'package:first_project/pages/widgets/drawer.dart';
 import 'package:first_project/pages/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    catelogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final DumyList = List.generate(50, ((index) => catelogModel.items[0]));
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -15,18 +39,22 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: DumyList.length,
-          //itemCount: catelogModel.items.length,
-          itemBuilder: (BuildContext context, index) {
-            return ItemWidget(
-              item: DumyList[index],
-              //item: catelogModel.items[index],
-            );
-          },
-        ),
+        child: (catelogModel.items != null && catelogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: catelogModel.items.length,
+                //itemCount: catelogModel.items.length,
+                itemBuilder: (BuildContext context, index) => ItemWidget(
+                  item: catelogModel.items[index],
+                  //item: catelogModel.items[index],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
   }
+
+  newMethod(loadData()) => loadData();
 }
--- first_project/lib/pages/models/catelog.dart
+++ first_project/lib/pages/models/catelog.dart
@@ -1,14 +1,15 @@
+import 'dart:convert';
+
 class catelogModel {
-  static final items = [
-    Item(
-      id: 1,
-      color: '#33505a',
-      desp: 'Apple i phone 12th genration',
-      image:
-          'https://swaptou.mu/wp-content/uploads/2020/12/shared-iphone-12-pro-graphite-01-900x1200-1.jpg',
-      name: 'i phone 12 pro',
-      price: 999,
-    )
+  static List<Item> items = [
+    // Item(
+    // id: 1,
+    // color: '#33505a',
+    // desp: 'Apple i phone 12th genration',
+    // image:
+    //     'https://swaptou.mu/wp-content/uploads/2020/12/shared-iphone-12-pro-graphite-01-900x1200-1.jpg',
+    // name: 'i phone 12 pro',
+    // price: 999, )
   ];
 }
 
@@ -20,11 +21,86 @@ class Item {
   final String color;
   final String image;
 
-  Item(
-      {required this.id,
-      required this.name,
-      required this.desp,
-      required this.price,
-      required this.color,
-      required this.image});
+  Item({
+    required this.id,
+    required this.name,
+    required this.desp,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desp,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desp: desp ?? this.desp,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'name': name});
+    result.addAll({'desp': desp});
+    result.addAll({'price': price});
+    result.addAll({'color': color});
+    result.addAll({'image': image});
+
+    return result;
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      desp: map['desp'] ?? '',
+      price: map['price'] ?? 0,
+      color: map['color'] ?? '',
+      image: map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desp: $desp, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Item &&
+        other.id == id &&
+        other.name == name &&
+        other.desp == desp &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desp.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
